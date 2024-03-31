f3a8479df285264ab2aa131789ff658414d429a7
abdul <raman.mohammed16@gmail.com>
2024-03-31 02:12:44 +0100

    BasketAPI Implementation

---
+++ src/Service/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+
+           
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetSection("DatabaseSettings:DatabaseName").Value);
+            Products = database.GetCollection<Product>(configuration.GetSection("DatabaseSettings:CollectionName").Value);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
